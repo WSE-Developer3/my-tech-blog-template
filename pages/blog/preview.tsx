71b582daa0ed3291179203d092fea9f1ff321675
Developer_of_Racsche <developer@racsche.com>
2021-09-01 18:08:37 +0900

    プレビュー機能の追加

--- pages/blog/preview.tsx
+++ pages/blog/preview.tsx
@@ -4,43 +4,56 @@ import { useRouter } from "next/router";
 
 export default function PostDetailPage() {
   const router = useRouter();
-  const { contentid, draftkey } = router.query;
+  // const [param, setParam] = useState<any>();
   const [post, setPost] = useState({
     title: "",
     body: "",
   });
   const [categoryList, setCategoryList] = useState([{ url: [""], title: "" }]);
 
-  useEffect(() => {
-    const main = async () => {
-      /** プレビュー記事取得 */
-      const fetchOptions = {
-        headers: {
-          "X-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
-        },
-      };
-      const responstPost = await fetch(
-        `https://paths-are-template.microcms.io/api/v1/blog/${contentid}?draftKey=${draftkey}`,
-        fetchOptions
-      );
-      const post = await responstPost.json();
-      setPost(post);
+  // useEffect(() => {
+  //   if (router.asPath !== router.route) {
+  //     setParam(router.query);
+  //   }
+  // }, [router]);
 
-      /** カテゴリー一覧 */
-      const responseCategoryList: any = await fetch(
-        `https://paths-are-template.microcms.io/api/v1/category?fields=id%2Ccategory`,
-        fetchOptions
-      );
-      const responseCategoryListJson: any = await responseCategoryList.json();
-      const categoryList = responseCategoryListJson.contents.map(
-        (content: any) => {
-          return { url: content.id, title: content.category };
-        }
-      );
-      setCategoryList(categoryList);
-    };
-    main();
-  }, []);
+  // idが取得されてセットされたら処理される
+  useEffect(() => {
+    if (router.isReady) {
+      // if (param) {
+      // ここでidの値を使用してデータをフェッチする
+      const { contentid, draftkey } = router.query;
+      const main = async () => {
+        /** プレビュー記事取得 */
+        const fetchOptions = {
+          headers: {
+            "X-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
+          },
+        };
+        const responstPost = await fetch(
+          `https://paths-are-template.microcms.io/api/v1/blog/${contentid}?draftKey=${draftkey}`,
+          fetchOptions
+        );
+        const post = await responstPost.json();
+        setPost(post);
+
+        /** カテゴリー一覧 */
+        const responseCategoryList: any = await fetch(
+          `https://paths-are-template.microcms.io/api/v1/category?fields=id%2Ccategory`,
+          fetchOptions
+        );
+        const responseCategoryListJson: any = await responseCategoryList.json();
+        const categoryList = responseCategoryListJson.contents.map(
+          (content: any) => {
+            return { url: content.id, title: content.category };
+          }
+        );
+        setCategoryList(categoryList);
+      };
+      main();
+      // }
+    }
+  }, [router.query]);
 
   return <PostDetailemplate post={post} categories={categoryList} />;
 }
