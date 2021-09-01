18ff9182e2507c789b2059fcbce72dc7fbcbb412
Developer_of_Racsche <developer@racsche.com>
2021-09-01 18:22:40 +0900

    プレビュー機能の追加

--- pages/blog/preview.tsx
+++ pages/blog/preview.tsx
@@ -4,35 +4,25 @@ import { useRouter } from "next/router";
 
 export default function PostDetailPage() {
   const router = useRouter();
-  // const [param, setParam] = useState<any>();
   const [post, setPost] = useState({
     title: "",
     body: "",
   });
   const [categoryList, setCategoryList] = useState([{ url: [""], title: "" }]);
 
-  // useEffect(() => {
-  //   if (router.asPath !== router.route) {
-  //     setParam(router.query);
-  //   }
-  // }, [router]);
-
   // idが取得されてセットされたら処理される
   useEffect(() => {
     if (router.isReady) {
-      // if (param) {
-      // ここでidの値を使用してデータをフェッチする
       const { contentid, draftkey } = router.query;
       const main = async () => {
         /** プレビュー記事取得 */
-        const fetchOptions = {
-          headers: {
-            "X-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
-          },
-        };
         const responstPost = await fetch(
           `https://paths-are-template.microcms.io/api/v1/blog/${contentid}?draftKey=${draftkey}`,
-          fetchOptions
+          {
+            headers: {
+              "X-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
+            },
+          }
         );
         const post = await responstPost.json();
         setPost(post);
@@ -40,7 +30,11 @@ export default function PostDetailPage() {
         /** カテゴリー一覧 */
         const responseCategoryList: any = await fetch(
           `https://paths-are-template.microcms.io/api/v1/category?fields=id%2Ccategory`,
-          fetchOptions
+          {
+            headers: {
+              "X-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY || "",
+            },
+          }
         );
         const responseCategoryListJson: any = await responseCategoryList.json();
         const categoryList = responseCategoryListJson.contents.map(
@@ -51,7 +45,6 @@ export default function PostDetailPage() {
         setCategoryList(categoryList);
       };
       main();
-      // }
     }
   }, [router.query]);
 
