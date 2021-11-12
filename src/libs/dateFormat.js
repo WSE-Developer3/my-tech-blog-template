8b8c1f17259aba614f60a97fc3da26b83a6a9c25
Shinkawa_Shinji <70325458+paths-are@users.noreply.github.com>
2021-11-12 12:10:13 +0900

    Update dateFormat.js
    post date revise

--- src/libs/dateFormat.js
+++ src/libs/dateFormat.js
@@ -1,4 +1,4 @@
 export const dateFormat = (value) => {
   const date = new Date(value);
-  return `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`;
+  return `${date.getFullYear()}年${date.getMonth()+1}月${date.getDate()}日`;
 };
